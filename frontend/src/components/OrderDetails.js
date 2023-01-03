068680b233d92155a2c50d806efe827d4f20d618
shovelkills <shovelkills@gmail.com>
2023-01-04 02:02:55 +1300

    Added MUI library

--- frontend/src/components/OrderDetails.js
+++ frontend/src/components/OrderDetails.js
@@ -6,6 +6,7 @@ const OrderDetails = ({ order }) =>{
 
     const { dispatch } = useOrdersContext()
 
+    //Function to delete data
     const handleClick = async () =>{
         const response = await fetch('/api/orders/' + order._id, {
             method: 'DELETE'
@@ -18,12 +19,37 @@ const OrderDetails = ({ order }) =>{
     }
 
     return(
-        <div className="order-details">
-            <h4>{order.department}</h4>
-            <p><strong>Extension: </strong>{order.extension}</p>
-            <p><strong>Date: </strong>{formatDistanceToNow(new Date(order.date), {addSuffix: true})}</p>
-            <span onClick={handleClick}>delete</span>
-        </div>
+        //Order detail display 
+        <>
+        <button class="accordion"> <h4>{order.department}</h4> </button>
+            <div className="order-details">
+                <h4>{order.department}</h4>
+                <p><strong>Extension: </strong>{order.extension}</p>
+                <p><strong>Date: </strong>{formatDistanceToNow(new Date(order.date), {addSuffix: true})}</p>
+                <span onClick={handleClick}>delete</span>
+            </div>
+
+        <script type="text/javascript">
+
+            let acc = document.getElementsByClassName("accordion");
+            let i;
+
+            for (i = 0; i < acc.length; i++){
+                acc[i].addEventListener("click", function(){
+                    this.classList.toggle("active");
+
+                    var panel = this.nextElementSibling;
+                    if (panel.style.display === "block"){
+                        panel.style.display = "none";
+                    }else{
+                        panel.style.display = "block";
+                    }
+                })
+            }
+
+        </script>
+        </>
+        
     )
 }
 export default OrderDetails
